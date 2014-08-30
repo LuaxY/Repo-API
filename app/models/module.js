2cce9fa326b76aa190d152671a35a49a5192d91e
LuaxY <yann.guineau@voidmx.net>
2014-08-30 23:08:33 +0200

    refactor(models:module): improve validator

--- app/models/module.js
+++ app/models/module.js
@@ -1,13 +1,38 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
+// name validators
+
+function name_format(value) {
+    return value.indexOf('_') != -1;
+}
+
+function name_size(value) {
+    return value.length > 3 && value.length < 20;
+}
+
+var name_validator = [
+    { validator: name_format, msg: 'module name must contain underscore' },
+    { validator: name_size, msg: 'module name must be greater than 3 and less than 20' }
+];
+
+// version validator
+
+function version_format() {
+
+}
+
+var version_validator = [
+    { validator: /\d.\d.\d/, msg: 'version is not in format x.x.x'}
+];
+
 var ModuleSchema = new Schema({
-    name: String,
+    name: { type: String, validate: name_validator },
     author: String,
     url: String,
     description: String,
     category: String,
-    version: String,
+    version: { type: String, validate: version_validator },
     dofusVersion: String,
     lastUpdateDate: String
 }, {
@@ -15,13 +40,4 @@ var ModuleSchema = new Schema({
 });
 
 var Module = mongoose.model('Module', ModuleSchema);
-
-Module.schema.path('name').validate(function(value) {
-    return value.length > 3 && value.length < 20;
-}, 'module name must be greater than 3 and less than 20');
-
-Module.schema.path('name').validate(function(value) {
-    return value.indexOf('_') != -1;
-}, 'module name must contain underscore');
-
 module.exports = Module;
